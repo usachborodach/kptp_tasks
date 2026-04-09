6233fb44b0f3b4b38aeac652be811a84d32bdc4b
gorbov <usachborodach@gmail.com>
2026-04-09 21:13:27 +0300

    new file:   favicon.ico

--- gen_html.py
+++ gen_html.py
@@ -24,6 +24,7 @@ def render_html(tasks, current_time):
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Задачи КПТП Дубровка (поле)</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <style>
         * {
             margin: 0;
